5b661d7ad3f876a79efc3ed26cc7c0abd36a004e
Aliaksandr Shyshonak <ashyshonak@infoblox.com>
2022-05-05 21:32:39 +0300

    feat: update search

--- src/app/courses/courses-toolbar/courses-toolbar.component.ts
+++ src/app/courses/courses-toolbar/courses-toolbar.component.ts
@@ -1,4 +1,8 @@
-import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angular/core';
+import { ChangeDetectionStrategy, Component, EventEmitter, OnDestroy, OnInit, Output } from '@angular/core';
+import { debounceTime, filter, Subject, tap } from 'rxjs';
+
+const SEARCH_DEBOUNCE_TIME = 200;
+const SEARCH_MIN_LENGTH = 3;
 
 @Component({
   selector: 'app-courses-toolbar',
@@ -6,11 +10,31 @@ import { ChangeDetectionStrategy, Component, EventEmitter, Output } from '@angul
   styleUrls: ['./courses-toolbar.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class CoursesToolbarComponent {
+export class CoursesToolbarComponent implements OnInit, OnDestroy {
   @Output() searchKey: EventEmitter<string> = new EventEmitter();
-  searchStr: string = '';
 
-  onSearchClick(): void {
-    this.searchKey.emit(this.searchStr);
+  private readonly destroy$ = new Subject<void>();
+  private readonly searchStr$: Subject<string> = new Subject();
+
+  ngOnInit(): void {
+    this.searchStr$
+      .asObservable()
+      .pipe(
+        filter((searchStr) => !searchStr || (!!searchStr && searchStr.length > SEARCH_MIN_LENGTH)),
+        debounceTime(SEARCH_DEBOUNCE_TIME),
+        tap((searchStr) => {
+          this.searchKey.emit(searchStr);
+        })
+      )
+      .subscribe();
+  }
+
+  ngOnDestroy(): void {
+    this.destroy$.next();
+    this.destroy$.complete();
+  }
+
+  onSearchKeyUp(input: KeyboardEvent): void {
+    this.searchStr$.next((input?.target as HTMLInputElement).value);
   }
 }
