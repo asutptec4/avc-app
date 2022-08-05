0e3f09570c6e8fc9d23e1b3b410db9dc8f94a7ca
Aliaksandr Shyshonak <ashyshonak@infoblox.com>
2022-08-05 10:09:52 +0300

    chore: update way of import

--- src/app/courses/services/courses-service.module.ts
+++ src/app/courses/services/courses-service.module.ts
@@ -2,11 +2,11 @@ import { NgModule } from '@angular/core';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 
-import { CoursesEffects, CoursesFacade, reducer } from '../state';
+import { CoursesEffects, CoursesFacade, coursesFeature } from '../state';
 import { CoursesDataService } from './courses-data.service';
 
 @NgModule({
-  imports: [StoreModule.forFeature('courses', reducer), EffectsModule.forFeature([CoursesEffects])],
+  imports: [StoreModule.forFeature(coursesFeature), EffectsModule.forFeature([CoursesEffects])],
   providers: [CoursesDataService, CoursesFacade]
 })
 export class CoursesServiceModule {}
